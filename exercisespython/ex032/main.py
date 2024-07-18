0e7eb40feb80cd14dfd70ebc414d20ec52436086
Mario Nelson <marionelson616@gmail.com>
2024-07-18 08:49:52 -0400

    att: exercise #032

--- exercisespython/ex032/main.py
+++ exercisespython/ex032/main.py
@@ -1,6 +1,10 @@
-ano = int(input('Informe um ano(ex. 2024) para saber se ele é bissexto ou não:\n'))
+from datetime import date
 
-if ano % 4 == 0:
+ano = int(input('Informe um ano para mais detalhes: (Digite "0"s para selecionar o ano atual)\n'))
+
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print(f'{ano} é um ano bissexto!')
 else:
     print(f'{ano} não é um ano bissexto.')
