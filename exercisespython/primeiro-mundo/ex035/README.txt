b0faf84cbbe7b89eb7c6bbe11da2f6f42fd9ff28
Dylan <marionelson616@gmail.com>
2024-07-21 20:36:59 -0400

    Adding exercise #035

---
+++ exercisespython/primeiro-mundo/ex035/README.txt
@@ -0,0 +1,7 @@
+Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou nao formar um triângulo.
+
+Raciocínio:
+
+1. Coletar o comprimento das três retas.
+2. Calcule se as medidas podem formar um triângulo.
+3. Printe o resultado para o usuário.
---
+++ exercisespython/primeiro-mundo/ex035/main.py
@@ -0,0 +1,8 @@
+retaa = float(input('Digite o comprimento da reta A: '))
+retab = float(input('Digite o comprimento da reta B: '))
+retac = float(input('Digite o comprimento da reta C: '))
+
+if retaa + retab > retac and retaa + retac > retab and retab + retac > retaa:
+    print(f'As retas com os seguintes comprimentos podem formar um triângulo:\nReta A: {retaa}\nReta B: {retab}\nReta C: {retac}')
+else:
+    print(f'As retas com os seguintes comprimentos NÃO podem formar um triângulo:\nReta A: {retaa}\nReta B: {retab}\nReta C: {retac}')
