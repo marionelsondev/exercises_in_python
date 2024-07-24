46822cd2dbdbde01608f57de2df0eaec2adc0c8a
Mario Nelson <marionelson616@gmail.com>
2024-07-24 08:43:21 -0400

    Adding exercise #037

--- exercisespython/segundo-mundo/ex037/main.py
+++ exercisespython/segundo-mundo/ex037/main.py
@@ -1,18 +1,17 @@
-num = int(input('Digite um número inteiro:'))
-option = int(input('Para qual base deseja converter?\n[1] BINÁRIO\n[2] OCTAL\n[3] HEXADECIMAL\n'))
-num_conv = []
+import utils
 
-def inverter_lista_slicing(lista):
-    return lista[::-1]
+num = int(input("Digite um número inteiro:"))
+option = int(input("Para qual base deseja converter?\n[1] BINÁRIO\n[2] OCTAL\n[3] HEXADECIMAL\n"))
+num_conv = 0
 
 if option == 1:
-    while num / 2 != 0:
-        resto = num % 2
-        num = num // 2
-        num_conv.append(resto)
-        inverter_lista_slicing(num_conv)
-    print(num_conv)
+    num_conv = utils.inteiro_binario(num)
+    print(f"O número {num} convertido para binário é: {num_conv}")
 elif option == 2:
-    print()
+    num_conv = utils.inteiro_octal(num)
+    print(f"O número {num} convertido para octal é: {num_conv}")
+elif option == 3:
+    num_conv = utils.inteiro_hexadecimal(num)
+    print(f"O número {num} convertido para hexadecimal é: {num_conv}")
 else:
-    print()
+    print("Opção inválida, por favor tente novamente.")
---
+++ exercisespython/segundo-mundo/ex037/utils.py
@@ -0,0 +1,8 @@
+def inteiro_binario(n):
+    return bin(n)[2:]
+
+def inteiro_octal(n):
+    return oct(n)[2:]
+
+def inteiro_hexadecimal(n):
+    return hex(n)[2:]
