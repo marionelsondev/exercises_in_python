4d3b6a078fddbb5ce423a0f02293c20f0e197347
Mario Nelson <marionelson616@gmail.com>
2024-07-31 08:36:09 -0400

    att v1.0 exercise #048

--- exercisespython/segundo-mundo/ex048/main.py
+++ exercisespython/segundo-mundo/ex048/main.py
@@ -1,8 +1,10 @@
 total_soma = 0
+total_numero = 0
 
 print("SOMADOR DE NÚMEROS MÚLTIPLOS DE TRÊS QUE SE ENCONTRAM NO INTERVALO DE 1 ATÉ 500")
 
-for numero in range(1, 501):
+for numero in range(1, 501, 2):
     if numero%3 == 0:
         total_soma += numero
-print(total_soma)
+        total_numero += 1
+print(f"A soma de todos os {total_numero} valores solicitados é {total_soma}")
