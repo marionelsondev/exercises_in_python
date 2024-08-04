9e0a77a2d1b29d07f4f1a0a821aa0eda83b685ec
¨Mario <marionelson616@gmail.com>
2024-08-03 21:11:38 -0300

    Att exercise #054

--- exercisespython/segundo-mundo/ex054/main.py
+++ exercisespython/segundo-mundo/ex054/main.py
@@ -1,13 +1,16 @@
 from datetime import date
 
+current_year = date.today().year
+totsmaller = 0
+totbigger = 0
 list_people = []
 
 for people in range(1, 8):
     birth_year = int(input(f"Enter the birth year of the {people}st person: "))
-    list_people.append(date.today().year - birth_year)
-print(f"{"-"*54}\nLIST OF PEOPLE WHO ARE ADULTS AND WHO ARE STILL MINORS\n{"-"*54}")
-for age in range(0, 7):
-    if list_people[age] < 21:
-        print(f"The {age+1}st person is {list_people[age]} years old and is still a minor.")
+    age = current_year - birth_year
+    if age < 21:
+        totsmaller += 1
     else:
-        print(f"The {age+1}st person is {list_people[age]} years old and is already an adult.")
+        totbigger += 1
+print(f"In total, we had {totsmaller} minors.")
+print(f"And we also had {totbigger} adults.")
