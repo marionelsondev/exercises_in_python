77d5f510218de0308ed13ff751245c0e5b3ba6bb
Mario Nelson <gbdev03@gbringel.com>
2024-09-04 08:21:35 -0300

    Adding exercise #072

---
+++ exercisespython/terceiro-mundo/ex072/README.txt
@@ -0,0 +1,7 @@
+Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso
+
+Raciocínio:
+
+1. Ler um valor entre 0 e 20. -
+2. Criar um tupla para armazenar os 20 valores por extenso. -
+3. Exibir para o usuário o número escolhido por extenso. -
---
+++ exercisespython/terceiro-mundo/ex072/main.py
@@ -0,0 +1,9 @@
+numeros = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
+cont = 0
+
+while True:
+    num = int(input("Digite um número entre 0 e 20: "))
+    if 0 <= num <= 20:
+        break
+    print("Tente novamente. ", end="")
+print(f"Você digitou o número {numeros[num]}")
